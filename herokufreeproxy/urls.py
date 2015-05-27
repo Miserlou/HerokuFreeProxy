e98ce29a7b1b0d4cb8e7cc6849205161a9409f71
Rich Jones <rich@anomos.info>
2015-05-26 19:27:32 -0700

    add proxy

--- herokufreeproxy/urls.py
+++ herokufreeproxy/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from proxy import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'herokufreeproxy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url('proxy/(?P<url>.*)', views.proxy_view),
     url(r'^admin/', include(admin.site.urls)),
 )
